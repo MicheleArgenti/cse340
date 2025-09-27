19fd1c428e15f1d4688e1c455505d3bded814f7d
raghork <michele.argenti000@gmail.com>
2025-09-27 12:49:36 +0200

    New changes

---
+++ routes/accountRoute.js
@@ -0,0 +1,20 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
+// Route to build path to login page
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Route to build path to register page
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+// Route to craete an account
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require("./database/")
+const account = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -22,6 +26,28 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+ }))
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
@@ -29,6 +55,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// Account
+app.use("/account", account)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
